a6e067528e5001940fc56091f0ece5dbeb09eaa3
Erica-Brock <Eady.Brock@gmail.com>
2017-10-06 08:55:36 -0500

    done

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-var suspects, weapons, where;
-suspect = ["Alice", "The Mad Hatter", "Cheshire Cat", "Tweedledum", "Tweedledee"];
-weapons = ["a light saber", "the zombie virus", "a mallet", "a thorn", "the song that never ends",
+var suspect = ["Alice", "The Mad Hatter", "Cheshire Cat", "Tweedledum", "Tweedledee"],
+    weapons = ["a light saber", "the zombie virus", "a mallet", "a thorn", "the song that never ends",
     "Avada Kedavra!", "Katnis' bow and arrow", "a rope", "a katana", "a chicken bone",
     "a piece of glass", "dragon fire", "pixie dust", "a baseball bat", "Medusa's eyes",
-    "King Arthur's sword", "the Autobots", "a phaser set to 'kill'", "Pan Galactic Gargle Blaster", "a dementor"];
-where = ["the Forbidden Forest", "the Cathedral", "the watchtower", "bed", "Vulcan airspace",
-    "the bridge of the Enterprise", "Red Mountaint Park", "the iLab", "the Garden", "Serendipity"];
+    "King Arthur's sword", "the Autobots", "a phaser set to 'kill'", "Pan Galactic Gargle Blaster", "a dementor"],
+    where = ["the Forbidden Forest", "the Cathedral", "the watchtower", "bed", "Vulcan airspace",
+    "the bridge of the Enterprise", "Red Mountain Park", "the iLab", "the Garden", "Serendipity"];
+
 //I accuse FRIEND_NAME, with the WEAPON_NAME in the LOCATION_NAME!
 document.addEventListener("DOMContentLoaded", function () {
     for (var i = 1; i <= 100; i++) {
-        accusations(i);
+        var randomSuspect = suspect[Math.floor(Math.random()*suspect.length)];
+        var randomWeapon = weapons[Math.floor(Math.random()*weapons.length)];
+        var randomLocation = where[Math.floor(Math.random()*where.length)];
+
+        var h3 = document.createElement('h3');
+        h3.innerText = 'Accusation ' + i;
+        document.body.appendChild(h3);
+
+        var clue = {
+            suspect: randomSuspect,
+            weapon: randomWeapon,
+            where: randomLocation
+        };
+
+        h3.addEventListener('click', message.bind(clue));
     }
-    function accusations(number) {
-        var accusation = document.createElement("h3");
-        accusation.innerHTML = "Accusation " + [i];
-        accusation.id = "accusation-" + [i];
-        document.body.appendChild(accusation);
+
+    function message(){
+        alert("I accuse "+ this.suspect+ " ,with "+ this.weapon +" in "+ this.where);
     }
-})
+});
